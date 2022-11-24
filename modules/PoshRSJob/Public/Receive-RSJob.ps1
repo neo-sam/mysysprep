d6ca6bf8bdeaff0f9d17d010c0368d826cc474d6
LittleboyHarry <littleboyharry@qq.com>
2022-11-24 11:36:37 +0800

    ! encoding fix

--- modules/PoshRSJob/Public/Receive-RSJob.ps1
+++ modules/PoshRSJob/Public/Receive-RSJob.ps1
@@ -1,1 +1,99 @@
-䙵湣瑩潮⁒散敩癥ⵒ半潢⁻ഊ††㰣ഊ††††⹓奎佐卉匍ਠ†††††⁇整猠瑨攠牥獵汴猠潦⁴桥⁗楮摯睳⁐潷敲卨敬氠牵湳灡捥⁪潢猠楮⁴桥⁣畲牥湴⁳敳獩潮⸍਍ਠ†††‮䑅千剉偔䥏不ਠ†††††⁇整猠瑨攠牥獵汴猠潦⁴桥⁗楮摯睳⁐潷敲卨敬氠牵湳灡捥⁪潢猠楮⁴桥⁣畲牥湴⁳敳獩潮⸠奯甠捡渠畳攍ਠ†††††⁇整ⵒ半潢⁡湤⁰楰攠瑨攠牥獵汴猠楮瑯⁴桩猠晵湣瑩潮⁴漠来琠瑨攠牥獵汴猠慳⁷敬氮ഊഊ††††⹐䅒䅍䕔䕒⁊潢ഊ††††††剥灲敳敮瑳⁴桥⁒半潢⁯扪散琠瑯⁲散敩癥⁡癡楬慢汥⁤慴愠晲潭⸍਍ਠ†††‮偁剁䵅呅删乡浥ഊ††††††周攠湡浥⁯映瑨攠橯扳⁴漠牥捥楶攠慶慩污扬攠摡瑡⁦牯洮ഊഊ††††⹐䅒䅍䕔䕒⁉䐍ਠ†††††⁔桥⁉䐠潦⁴桥⁪潢猠瑯⁲散敩癥⁡癡楬慢汥⁤慴愠晲潭⸍਍ਠ†††‮偁剁䵅呅删䥮獴慮捥䥄ഊ††††††周攠䝕䥄⁯映瑨攠橯扳⁴漠牥捥楶攠慶慩污扬攠摡瑡⁦牯洮ഊഊ††††⹐䅒䅍䕔䕒⁂慴捨ഊ††††††乡浥⁯映瑨攠獥琠潦⁪潢猠瑯⁲散敩癥⁡癡楬慢汥⁤慴愠晲潭⸍਍ਠ†††‮乏呅匍ਠ†††††⁎慭攺⁒散敩癥ⵒ半潢ഊ††††††䅵瑨潲㨠䉯攠偲潸⽍慸⁋潺汯瘍਍ਠ†††‮䕘䅍偌䔍ਠ†††††⁇整ⵒ半潢‭却慴攠䍯浰汥瑥搠簠剥捥楶攭剓䩯戍਍ਠ†††††⁄敳捲楰瑩潮ഊ††††††ⴭⴭⴭⴭⴭⴍਠ†††††⁒整物敶敳⁡湹⁡癡楬慢汥⁤慴愠瑨慴⁩猠潵瑰畴瑥搠晲潭⁣潭灬整敤⁒半潢献ഊഊ††††⹅塁䵐䱅ഊ††††††剥捥楶攭剓䩯戠ⵉ䐠ㄬ㔬㜸ഊഊ††††††䑥獣物灴楯渍ਠ†††††‭ⴭⴭⴭⴭⴭഊ††††††剥捥楶敳⁤慴愠晲潭⁒半潢⁷楴栠䥄猠ㄬ㔬㜸⸍਍ਠ†††‮䕘䅍偌䔍ਠ†††††⁒散敩癥ⵒ半潢‭䥮灵瑏扪散琠⡇整ⵒ半潢⤍਍ਠ†††††⁄敳捲楰瑩潮ഊ††††††ⴭⴭⴭⴭⴭⴍਠ†††††⁒散敩癥猠摡瑡⁦牯洠慬氠剓䩯扳⸍ਠ†‣㸍ਠ†⁛捭摬整扩湤楮木ഊ††††䑥晡畬瑐慲慭整敲卥瑎慭攽❊潢✍ਠ† 崍ਠ†⁐慲慭 ഊ††††孰慲慭整敲⡖慬略䙲潭偩灥汩湥㴤呲略ⱖ慬略䙲潭偩灥汩湥䉹偲潰敲瑹乡浥㴤呲略Ⰽਠ†††⁐慲慭整敲卥瑎慭攽❊潢✬⁐潳楴楯渽〩崍ਠ†††⁛䅬楡猨❉湰畴佢橥捴✩崍ਠ†††⁛剓䩯扛嵝⑊潢Ⰽਠ†††⁛灡牡浥瑥爨噡汵敆牯浐楰敬楮攽⑔牵攬噡汵敆牯浐楰敬楮敂祐牯灥牴祎慭攽⑔牵攬ഊ††††偡牡浥瑥牓整乡浥㴧乡浥✬⁐潳楴楯渽〩崍ਠ†††⁛獴物湧孝崤乡浥Ⰽਠ†††⁛灡牡浥瑥爨噡汵敆牯浐楰敬楮攽⑔牵攬噡汵敆牯浐楰敬楮敂祐牯灥牴祎慭攽⑔牵攬ഊ††††偡牡浥瑥牓整乡浥㴧䥤✬⁐潳楴楯渽〩崍ਠ†††⁛楮瑛嵝⑉搬ഊ††††孰慲慭整敲⡖慬略䙲潭偩灥汩湥䉹偲潰敲瑹乡浥㴤呲略Ⰽਠ†††⁐慲慭整敲卥瑎慭攽❉湳瑡湣敉䐧⥝ഊ††††孳瑲楮杛嵝⑉湳瑡湣敉䐬ഊ††††孰慲慭整敲⡖慬略䙲潭偩灥汩湥䉹偲潰敲瑹乡浥㴤呲略Ⰽਠ†††⁐慲慭整敲卥瑎慭攽❂慴捨✩崍ਠ†††⁛獴物湧孝崤䉡瑣栍ਠ† ഊ††䉥杩渠笍ਠ†††⁉映⠤偓䉯畮摐慲慭整敲獛❄敢畧❝⤠笍ਠ†††††․䑥扵材牥晥牥湣攠㴠❃潮瑩湵攧ഊ††††納ਠ†††․䱩獴‽⁎敷ⵏ扪散琠卹獴敭⹃潬汥捴楯湳⹁牲慹䱩獴ഊ††納ਠ†⁐牯捥獳⁻ഊ††††坲楴攭䑥扵朠≐慲慭整敲卥琺․⠤偓䍭摬整⹐慲慭整敲卥瑎慭攩∍ਠ†††․偲潰敲瑹‽․偓䍭摬整⹐慲慭整敲卥瑎慭攍ਠ†††⁩映⠤偓䉯畮摐慲慭整敲獛⑐牯灥牴祝⤠笍ਠ†††††⁗物瑥ⵖ敲扯獥•䅤摩湧․⠤偓䉯畮摐慲慭整敲獛⑐牯灥牴祝⤢ഊ††††††孶潩摝⑌楳琮䅤摒慮来⠤偓䉯畮摐慲慭整敲獛⑐牯灥牴祝⤍ਠ†††⁽ഊ††納ਠ†⁅湤⁻ഊ††††楦 ⵮潴․䱩獴⹃潵湴⤠笠牥瑵牮⁽‣⁎漠橯扳⁳敬散瑥搠瑯⁳敡牣栍ਠ†††․偓䉯畮摐慲慭整敲獛⑐牯灥牴祝‽․䱩獴ഊ††††孡牲慹崤呯剥捥楶攠㴠䝥琭剓䩯戠䁐卂潵湤偡牡浥瑥牳ഊഊ††††楦 ⑔潒散敩癥⹃潵湴⤠笍ਠ†††††․呯剥捥楶攠簠䙯牅慣栭佢橥捴笍ਠ†††††††․张簠坲楴敓瑲敡洍ਠ†††††††⁩映⡀⠢䍯浰汥瑥搢Ⱐ≆慩汥搢Ⱐ≓瑯灰敤∩‭捯湴慩湳․弮却慴攩⁻ഊ††††††††††⑟⁼⁓整䥳剥捥楶敤‭卥瑔牵攍ਠ†††††††⁽ഊ††††††納ਠ†††⁽ഊ††納੽ഊ
+Function Receive-RSJob {
+    <#
+        .SYNOPSIS
+            Gets the results of the Windows PowerShell runspace jobs in the current session.
+
+        .DESCRIPTION
+            Gets the results of the Windows PowerShell runspace jobs in the current session. You can use
+            Get-RSJob and pipe the results into this function to get the results as well.
+
+        .PARAMETER Job
+            Represents the RSJob object to receive available data from.
+
+        .PARAMETER Name
+            The name of the jobs to receive available data from.
+
+        .PARAMETER ID
+            The ID of the jobs to receive available data from.
+
+        .PARAMETER InstanceID
+            The GUID of the jobs to receive available data from.
+
+        .PARAMETER Batch
+            Name of the set of jobs to receive available data from.
+
+        .NOTES
+            Name: Receive-RSJob
+            Author: Boe Prox/Max Kozlov
+
+        .EXAMPLE
+            Get-RSJob -State Completed | Receive-RSJob
+
+            Description
+            -----------
+            Retrieves any available data that is outputted from completed RSJobs.
+
+        .EXAMPLE
+            Receive-RSJob -ID 1,5,78
+
+            Description
+            -----------
+            Receives data from RSJob with IDs 1,5,78.
+
+        .EXAMPLE
+            Receive-RSJob -InputObject (Get-RSJob)
+
+            Description
+            -----------
+            Receives data from all RSJobs.
+    #>
+    [cmdletbinding(
+        DefaultParameterSetName = 'Job'
+    )]
+    Param (
+        [parameter(ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $True,
+            ParameterSetName = 'Job', Position = 0)]
+        [Alias('InputObject')]
+        [RSJob[]]$Job,
+        [parameter(ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $True,
+            ParameterSetName = 'Name', Position = 0)]
+        [string[]]$Name,
+        [parameter(ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $True,
+            ParameterSetName = 'Id', Position = 0)]
+        [int[]]$Id,
+        [parameter(ValueFromPipelineByPropertyName = $True,
+            ParameterSetName = 'InstanceID')]
+        [string[]]$InstanceID,
+        [parameter(ValueFromPipelineByPropertyName = $True,
+            ParameterSetName = 'Batch')]
+        [string[]]$Batch
+    )
+    Begin {
+        If ($PSBoundParameters['Debug']) {
+            $DebugPreference = 'Continue'
+        }
+        $List = New-Object System.Collections.ArrayList
+    }
+    Process {
+        Write-Debug "ParameterSet: $($PSCmdlet.ParameterSetName)"
+        $Property = $PSCmdlet.ParameterSetName
+        if ($PSBoundParameters[$Property]) {
+            Write-Verbose "Adding $($PSBoundParameters[$Property])"
+            [void]$List.AddRange($PSBoundParameters[$Property])
+        }
+    }
+    End {
+        if (-not $List.Count) { return } # No jobs selected to search
+        $PSBoundParameters[$Property] = $List
+        [array]$ToReceive = Get-RSJob @PSBoundParameters
+
+        if ($ToReceive.Count) {
+            $ToReceive | ForEach-Object {
+                $_ | WriteStream
+                if (@("Completed", "Failed", "Stopped") -contains $_.State) {
+                    $_ | SetIsReceived -SetTrue
+                }
+            }
+        }
+    }
+}
